8ec118090ea517eb43bae7260e267b5fd64175a2
Bjarne <bjarne_priebe@hotmail.com>
2022-02-18 20:19:43 +0100

    for each ConditionType

--- assignment1-main/src/main/MachineInterpreter.java
+++ assignment1-main/src/main/MachineInterpreter.java
@@ -3,6 +3,7 @@ package main;
 import main.metamodel.Machine;
 import main.metamodel.State;
 import main.metamodel.Transition;
+import main.metamodel.Transition.ConditionType;
 
 public class MachineInterpreter {
 	private Machine machine;
@@ -24,43 +25,44 @@ public class MachineInterpreter {
 			Integer operationVariableValue = machine.integers.get(operationVariableName);
 
 			if (currentTransition.conditionTypes.size() > 0) {
-				switch (currentTransition.conditionTypes.get(0)) {
-				case DECREMENT:
-					machine.integers.put(operationVariableName, operationVariableValue - 1);
-					break;
-				case INCREMENT:
-					machine.integers.put(operationVariableName, operationVariableValue + 1);
-					break;
-				case SET:
-					machine.integers.put(operationVariableName, currentTransition.operationVariableValue);
-					currentState = currentTransition.getTarget();
-					break;
-				case IFEQUALS:
-					if (currentTransition.conditionVariableValue.equals(operationVariableValue)) {
+				for (ConditionType condition : currentTransition.conditionTypes) {
+					switch (condition) {
+					case DECREMENT:
+						machine.integers.put(operationVariableName, operationVariableValue - 1);
+						break;
+					case INCREMENT:
+						machine.integers.put(operationVariableName, operationVariableValue + 1);
+						break;
+					case SET:
+						machine.integers.put(operationVariableName, currentTransition.operationVariableValue);
 						currentState = currentTransition.getTarget();
-					}
-					break;
-				case IFLESSTHAN:
-					if (operationVariableValue < currentTransition.conditionVariableValue) {
+						break;
+					case IFEQUALS:
+						if (currentTransition.conditionVariableValue.equals(operationVariableValue)) {
+							currentState = currentTransition.getTarget();
+						}
+						break;
+					case IFLESSTHAN:
+						if (operationVariableValue < currentTransition.conditionVariableValue) {
+							currentState = currentTransition.getTarget();
+						}
+						break;
+					case IFGREATERTHAN:
+						if (operationVariableValue > currentTransition.conditionVariableValue) {
+							currentState = currentTransition.getTarget();
+						}
+						break;
+					default:
 						currentState = currentTransition.getTarget();
+						break;
 					}
-					break;
-				case IFGREATERTHAN:
-					if (operationVariableValue > currentTransition.conditionVariableValue) {
-						currentState = currentTransition.getTarget();
-					}
-					break;
-				default:
-					currentState = currentTransition.getTarget();
-					break;
 				}
 			} else {
 				currentState = currentTransition.getTarget();
 			}
 		}
-
 	}
-
+	
 	public int getInteger(String string) {
 		return machine.integers.get(string);
 	}
